dcbd248e0ae2a0843dc569da857354c294bf88fb
KIRITHIHA R <kirithiha.r@revature.com>
2021-08-04 01:24:17 -0700

    logging levels

--- src/main/java/com/revature/exceptions/NotFoundException.java
+++ src/main/java/com/revature/exceptions/NotFoundException.java
@@ -1,11 +1,15 @@
 package com.revature.exceptions;
 
+import org.apache.log4j.Logger;
+
 public class NotFoundException extends Exception{
-	
+
 	private static final long serialVersionUID = 1L;
 
+	static Logger log = Logger.getLogger("NotFoundException.class");
 	public NotFoundException(String msg){
 		super(msg);
+		log.warn("Inside CUSTOM EXCEPTION CLASS");
 	}
 	
 }
